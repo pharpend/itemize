e46eeebcacc8a91c18c46bdafcd7378917955108
Peter Harpending <pharpend2@gmail.com>
2014-11-14 17:31:30 -0700

    initial commit, add itemize script and linux binary.

---
+++ itemthing.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import           Control.Applicative
+import           Data.Monoid
+import           Data.Traversable
+import           System.IO
+import           Text.Blaze.Html5 ((!))
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import           Text.Blaze.Html.Renderer.String
+
+doSeason :: Int -> IO H.Html
+doSeason season = do
+  hPutStr stderr $ "How many episodes in season " ++ show season ++ "? "
+  hFlush stderr
+  (numEpisodes :: Int) <- read <$> getLine
+  _ <- if numEpisodes <= 0
+          then fail "not enough episodes"
+          else return ()
+  return $ mkSeason season numEpisodes
+
+mkSeason :: Int -> Int -> H.Html
+mkSeason season numEpisodes = do
+  H.li ! A.class_ "item-has-children" $ do
+    H.a ! A.href "#0" $ do
+      H.toHtml $ "Season " ++ show season
+    H.ul ! A.class_ "sub-menu" $ do
+      mconcat $ map (mkEpisode season) [1 .. numEpisodes]
+
+mkEpisode :: Int -> Int -> H.Html
+mkEpisode season epinum = do
+  H.li $ do
+    H.a ! A.href "#" ! A.id (H.toValue $  "S" ++ show season ++ "E" ++ show epinum) $ do
+      H.toHtml $ "Episode " ++ show epinum
+
+main :: IO ()
+main = do
+  hPutStr stderr "How many seasons? "
+  hFlush stderr
+  (numSeasons :: Int) <- read <$> getLine
+  _ <- if numSeasons <= 0
+          then fail "not enough seasons"
+          else return ()
+  seasons <- mconcat <$> forM [1 .. numSeasons] doSeason
+  putStr $ renderHtml seasons
